2450f089a1a3d2fe8bf092b9cdb80783c2e579f6
Sinem <114805159+sinemcskun@users.noreply.github.com>
2025-02-11 10:44:06 +0300

    Add files via upload
    Clear all feature is added
    Filled circle is fixed
    Change size of eraser and brush with mouse wheel added

--- paint.c
+++ paint.c
@@ -25,8 +25,18 @@ Color palette[] = {
 SDL_Color currentColor = {0,0,0,255}; // default color is black
 
 SDL_Texture* canvasTexture = NULL;
+int brushsize = 5; // brush or eraser size
 
 void SDL_RenderDrawCircle(SDL_Renderer* renderer, int x, int y, int radius){
+  for(int angle = 0; angle < 360; angle++){
+    float radian = angle * M_PI / 180.0f;
+    int dx = x + (int)(radius * cos(radian));
+    int dy = y + (int)(radius * sin(radian));
+    SDL_RenderDrawPoint(renderer, dx, dy);
+  }
+}
+
+void FillCircle(SDL_Renderer* renderer, int x, int y, int radius){
   for(int w = 0; w < radius * 2; w++){
     for(int h = 0; h < radius * 2; h++){
       int dx = radius - w;
@@ -36,6 +46,11 @@ void SDL_RenderDrawCircle(SDL_Renderer* renderer, int x, int y, int radius){
       }
     }
   }
+}
+
+void FillRectangle(SDL_Renderer* renderer, int x, int y, int w, int h){
+  SDL_RenderDrawLine(renderer, x, y, x + w, y + h);
+  //devam edilecek
 }
 
 typedef enum{
@@ -122,7 +137,8 @@ int main(){
         if(drawing){
           SDL_SetRenderTarget(renderer, canvasTexture); // use canvas for painting
           SDL_SetRenderDrawColor(renderer, currentColor.r, currentColor.g, currentColor.b, currentColor.a);
-          SDL_RenderDrawLine(renderer, previousPoint.x, previousPoint.y, event.motion.x, event.motion.y);
+          FillCircle(renderer, event.motion.x, event.motion.y, brushsize);
+          //SDL_RenderDrawLine(renderer, previousPoint.x, previousPoint.y, event.motion.x, event.motion.y);
           SDL_SetRenderTarget(renderer, NULL);
 
           previousPoint.x = event.motion.x;
@@ -130,12 +146,25 @@ int main(){
         } else if(erasing){
           SDL_SetRenderTarget(renderer, canvasTexture); // erase process be done on canvas
           SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-          SDL_RenderDrawLine(renderer, previousPoint.x, previousPoint.y, event.motion.x, event.motion.y);
+          FillCircle(renderer, event.motion.x, event.motion.y, brushsize);
+          //SDL_RenderDrawLine(renderer, previousPoint.x, previousPoint.y, event.motion.x, event.motion.y);
           SDL_SetRenderTarget(renderer, NULL);
 
           previousPoint.x = event.motion.x;
           previousPoint.y = event.motion.y;
         }
+      }else if(event.type == SDL_MOUSEWHEEL){
+        if(event.wheel.y >0){
+          brushsize += 2;
+        } else if(event.wheel.y < 0){
+          brushsize -= 2;
+        }
+        if(brushsize < 1){
+          brushsize = 1;
+        }else if(brushsize > 50){
+          brushsize = 50;
+        }
+
       }else if(event.type == SDL_KEYDOWN){
         if(event.key.keysym.sym == SDLK_s){
           SDL_Surface* saveSurface = SDL_CreateRGBSurfaceWithFormat(0, WINDOW_WIDTH, WINDOW_HEIGHT, 32, SDL_PIXELFORMAT_RGBA8888);
@@ -154,6 +183,11 @@ int main(){
           currentMode = MODE_RECTANGLE;
         } else if(event.key.keysym.sym == SDLK_SPACE){
           running = 0;
+        } else if(event.key.keysym.sym == SDLK_DELETE){
+          SDL_SetRenderTarget(renderer, canvasTexture);
+          SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+          SDL_RenderClear(renderer);
+          SDL_SetRenderTarget(renderer, NULL);
         }
       }
     }
